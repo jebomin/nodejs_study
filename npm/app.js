7b3c80a7621dd5c7f37631c99b042ba07832edbf
Bomin <42407430+jebomin@users.noreply.github.com>
2023-07-01 15:58:40 +0900

    installing in development mode, installing good tools

--- npm/app.js
+++ npm/app.js
@@ -1,8 +1,2 @@
 console.log("hi");
-
-const _ = require("underscore");
-
-// _ libray는 대체품이 있어서 필요 없음
-//array.forEach(num, => ) 이거로
-const array = [1, 2];
-_.forEach(array, (num) => console.log(num));
+console.log("안녕!");
