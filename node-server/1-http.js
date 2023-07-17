6c9a42120674aa8347edf6f7c866a4d3ab78d4aa
jebomin <jebomin0515@naver.com>
2023-07-17 22:06:03 +0900

    Send html content

--- node-server/1-http.js
+++ node-server/1-http.js
@@ -1,5 +1,5 @@
-const { log } = require("console");
 const http = require("http");
+const fs = require("fs");
 //const http2 = require('http2'); //https 배포할때
 
 const server = http.createServer((req, res) => {
@@ -8,8 +8,15 @@ const server = http.createServer((req, res) => {
   console.log(req.httpVersion);
   console.log(req.method);
   console.log(req.url);
-  res.write("Welcome!");
-  res.end();
+  res.setHeader("Contetn-Type", "text/html");
+  const url = req.url;
+  if (url === "/") {
+    const read = fs.createReadStream("./html/index.html").pipe(res);
+  } else if (url === "/courses") {
+    const read = fs.createReadStream("./html/courses.html").pipe(res);
+  } else {
+    const read = fs.createReadStream("./html/not-found.html").pipe(res);
+  }
 });
 
 server.listen(8080);
