2ea04e894d382f72978a425bcc630ea6d19358fa
jebomin <jebomin0515@naver.com>
2023-07-23 22:10:06 +0900

    Important Features of Middleware

--- 6-express/app.js
+++ 6-express/app.js
@@ -1,16 +1,41 @@
 import express from "express";
 const app = express();
 
-app.get("/sky/:id", (req, res, next) => {
-  //  console.log(req.path);
-  //  console.log(req.headers);
-  console.log(req.params);
-  console.log(req.params.id);
-  console.log(req.query);
-  console.log(req.query.keyword);
-
-  res.setHeader("key", "value");
-  res.status(201).send("created");
+app.all("/api/*", (req, res, next) => {
+  console.log("all");
+  next();
 });
+
+app.use("/sky", (req, res, next) => {
+  console.log("use");
+  next();
+});
+
+app.get(
+  "/",
+  (req, res, next) => {
+    console.log("first");
+    if (true) {
+      return res.send("Hello");
+    }
+    res.send("Bomin");
+  },
+  (req, res, next) => {
+    console.log("first2");
+  }
+);
+
+app.get("/", (req, res, next) => {
+  console.log("second");
+});
+
+app.use((req, res, next) => {
+  res.status(404).send("Not available!@_@");
+});
+
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.status(500).send("Sorry, try later!");
+}); //에러 처리 핸들러
 
 app.listen(8080);
