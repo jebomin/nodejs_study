8e0e19d22fbbf761011f2f13c6d92dc822a7ff5f
jebomin <jebomin0515@naver.com>
2023-07-24 09:29:56 +0900

    handling post

--- 6-express/app.js
+++ 6-express/app.js
@@ -1,41 +1,9 @@
 import express from "express";
 const app = express();
 
-app.all("/api/*", (req, res, next) => {
-  console.log("all");
-  next();
+app.use(express.json());
+app.post("/api/*", (req, res, next) => {
+  console.log(req.body);
 });
-
-app.use("/sky", (req, res, next) => {
-  console.log("use");
-  next();
-});
-
-app.get(
-  "/",
-  (req, res, next) => {
-    console.log("first");
-    if (true) {
-      return res.send("Hello");
-    }
-    res.send("Bomin");
-  },
-  (req, res, next) => {
-    console.log("first2");
-  }
-);
-
-app.get("/", (req, res, next) => {
-  console.log("second");
-});
-
-app.use((req, res, next) => {
-  res.status(404).send("Not available!@_@");
-});
-
-app.use((error, req, res, next) => {
-  console.error(error);
-  res.status(500).send("Sorry, try later!");
-}); //에러 처리 핸들러
 
 app.listen(8080);
