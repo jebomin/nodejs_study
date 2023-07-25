5d7d43581e73345d2ed2264946a49f6dc48cae1b
jebomin <jebomin0515@naver.com>
2023-07-25 15:08:16 +0900

    Useful internal middleware

--- 6-express/app-route.js
+++ 6-express/app-route.js
@@ -1,12 +1,27 @@
 import express from "express";
-import userRouter from "./router/user.js";
-import postRouter from "./router/post.js";
 
 const app = express();
 
+// express.json -> REST API, body parse
+// express.urlencoded -> HTML form
+// express.static
+
 app.use(express.json());
+app.post("/posts", (req, res) => {
+  console.log(req.body);
+  res.status(201).send("Thanks, Created");
+});
 
-app.use("/users", userRouter);
-app.use("/posts", postRouter);
+const options = {
+  dotfiles: "ignore",
+  etag: false,
+  index: false,
+  maxAge: "1d",
+  redirect: false,
+  setHeaders: function (res, path, stat) {
+    res.set("x-timestamp", Date.now());
+  },
+};
 
+app.use(express.static("public", options));
 app.listen(8080);
